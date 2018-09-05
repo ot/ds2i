cb16d83519892f44af8db074104a4ce8e90e32eb
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-09-05 16:57:12 -0400

    Tool for sampling index

---
+++ src/sample_index.cpp
@@ -0,0 +1,22 @@
+#include "CLI/CLI.hpp"
+#include "pstl/execution"
+#include "tbb/task_scheduler_init.h"
+
+#include "recursive_graph_bisection.hpp"
+#include "util/progress.hpp"
+
+int main(int argc, char const *argv[]) {
+
+    std::string input_basename;
+    std::string output_basename;
+    size_t      num_docs;
+
+    CLI::App app{"Sample n documents and store in a separate inverted index."};
+    app.add_option("-c,--collection", input_basename, "Collection basename")->required();
+    app.add_option("-o,--output", output_basename, "Output basename")->required();
+    app.add_option("-n,--num-doc", num_docs, "Number of documents")->required();
+    CLI11_PARSE(app, argc, argv);
+
+    ds2i::sample_inverted_index(input_basename, output_basename, num_docs);
+    return 0;
+}
